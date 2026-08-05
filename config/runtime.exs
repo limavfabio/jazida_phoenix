85879aef9fda2075a218967a0f41fbb83f615baf
limavfabio <limavfabio@gmail.com>
2026-08-05 19:45:10 -0300

    Fix release runtime configuration

--- config/runtime.exs
+++ config/runtime.exs
@@ -59,12 +59,12 @@ if config_env() == :dev do
       web_console_logger: true,
       patterns: [
         # Static assets, except user uploads
-        ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+        ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
         # Gettext translations
-        ~r"priv/gettext/.*\.po$"E,
+        ~r"priv/gettext/.*\.po$",
         # Router, Controllers, LiveViews and LiveComponents
-        ~r"lib/jazida_phoenix_web/router\.ex$"E,
-        ~r"lib/jazida_phoenix_web/(controllers|live|components)/.*\.(ex|heex)$"E
+        ~r"lib/jazida_phoenix_web/router\.ex$",
+        ~r"lib/jazida_phoenix_web/(controllers|live|components)/.*\.(ex|heex)$"
       ]
     ]
 end
